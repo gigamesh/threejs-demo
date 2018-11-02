ed3e6743f579eac1f461d34b596df164b3617aac
Matt Masurka <m.masurka@gmail.com>
2018-11-02 12:47:53 -0700

    cleans up App file

--- src/App.js
+++ src/App.js
@@ -12,84 +12,110 @@ class App extends Component {
   };
 
   componentDidMount() {
+    ////////////////// RENDERER
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
       antialias: true
     });
+
     const renderer = this.renderer;
     renderer.setClearColor(0xffffff);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+    ////////////////// CAMERA
     this.camera = new THREE.PerspectiveCamera(
       35,
       window.innerWidth / window.innerHeight,
       0.1,
       3000
     );
+
+    ////////////////// SCENE
     this.scene = new THREE.Scene();
     const scene = this.scene;
 
     const light1 = new THREE.AmbientLight(0x85c210, 1.5);
     scene.add(light1);
-    this.light2 = new THREE.PointLight(0x22ffffff, 0.5, 0, 2);
+    this.light2 = new THREE.PointLight(0xffffff, 0.4, 0, 2);
     const light2 = this.light2;
     scene.add(light2);
     const light3 = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
-    // light3.position.set(50, 100, 1000);
     scene.add(light3);
 
-    const loader = new THREE.FontLoader();
-    const font = loader.parse(fontJSON);
+    ////////////////// LOADER
+    const loader = new THREE.SVGLoader();
+    loader.load(
+      "assets/svg/Hom_logo3a.svg",
+      createGeometry,
+      xhr => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      error => {
+        console.log("An error happened");
+      }
+    );
 
-    const geometry = new THREE.TextGeometry("HOUSE OF MOVES", {
-      font,
-      size: 40,
-      height: 20,
-      bevelThickness: 20,
-      material: 10
-    });
+    ///////////////// GEOMETRY
 
-    geometry.translate(-250, 0, 0);
-    // geometry.rotateY(0.25);
-    geometry.rotateX(-0.2);
-    // geometry.rotateZ(0.5);
-    this.material = new THREE.MeshStandardMaterial({
-      color: 0x6ebf42,
-      // emissive: 0x0b3307,
-      // emissiveIntensity: 0.1,
-      metalness: 0.5,
-      roughness: 0.5,
-      wireframe: false
-    });
-    this.mesh = new THREE.Mesh(geometry, this.material);
-    const mesh = this.mesh;
-    mesh.position.set(0, 0, -500);
+    const createGeometry = paths => {
+      const group = new THREE.Group();
 
-    scene.add(mesh);
+      for (let i = 0; i < paths.length; i++) {
+        const path = paths[i];
+
+        const material = new THREE.MeshStandardMaterial({
+          color: 0xffffff,
+          side: THREE.DoubleSide,
+          depthWrite: false,
+          metalness: 0.5,
+          roughness: 0.5
+        });
+
+        const shapes = path.toShapes(true);
+
+        for (let j = 0; j < shapes.length; j++) {
+          const shape = shapes[j];
+          const geometry = new THREE.ShapeBufferGeometry(shape);
+          const mesh = new THREE.Mesh(geometry, material);
+          group.add(mesh);
+        }
+      }
+
+      scene.add(group);
+    };
+    // const geometry = new THREE.TextGeometry("HOUSE OF MOVES", {
+    //   font,
+    //   size: 40,
+    //   height: 20,
+    //   bevelThickness: 20,
+    //   material: 10
+    // });
+
+    // geometry.translate(-250, 0, 0);
+    // geometry.rotateX(-0.2);
+    // this.material = new THREE.MeshStandardMaterial({
+    //   color: 0x6ebf42,
+    //   // emissive: 0x0b3307,
+    //   // emissiveIntensity: 0.1,
+    //   metalness: 0.5,
+    //   roughness: 0.5,
+    //   wireframe: false
+    // });
+
+    ///////////////// MESH
+    // this.mesh = new THREE.Mesh(geometry, this.material);
+    // const mesh = this.mesh;
+    // mesh.position.set(0, 0, -500);
+
+    // scene.add(mesh);
 
     requestAnimationFrame(this.animate);
 
-    this.glitchTimer = setInterval(this.glitch, 5000);
+    // this.glitchTimer = setInterval(this.glitch, 4000);
   }
 
-  glitch = () => {
-    this.material.wireframe = true;
-    setTimeout(() => {
-      this.material.wireframe = false;
-      setTimeout(() => {
-        this.material.wireframe = true;
-        setTimeout(() => {
-          this.material.wireframe = false;
-        }, 50);
-      }, 50);
-    }, 50);
-  };
-
-  delta = 0;
   animate = () => {
-    let wave = Math.sin((this.delta += 0.01));
-    this.mesh.rotation.y += wave * 0.001;
     this.light2.position.set(
       this.state.mouseX - 800,
       this.state.mouseY - 300,
@@ -100,7 +126,6 @@ class App extends Component {
   };
 
   mouseMove = e => {
-    // console.log(e.nativeEvent.offsetX);
     this.setState({
       mouseX: e.nativeEvent.offsetX,
       mouseY: e.nativeEvent.offsetY
@@ -108,6 +133,7 @@ class App extends Component {
   };
 
   render() {
+    // console.log(this.state.mouseX, this.state.mouseY);
     return (
       <canvas ref={el => (this.canvas = el)} onMouseMove={this.mouseMove} />
     );
